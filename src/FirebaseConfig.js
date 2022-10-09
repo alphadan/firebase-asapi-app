f737e49f537a90447cdfea7d808ab10c5ce81d3c
Daniel Keane <info@alphabetsigns.com>
2022-10-09 11:51:36 -0400

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAn6cm8cXGWS4z_VwkVLFKinAN5_oBFshU",
-  authDomain: "fir-asapi.firebaseapp.com",
-  projectId: "fir-asapi",
-  storageBucket: "fir-asapi.appspot.com",
-  messagingSenderId: "114853796331",
-  appId: "1:114853796331:web:47a73bac0c42c606e1a1ae",
+
 };
 
 const app = initializeApp(firebaseConfig);
