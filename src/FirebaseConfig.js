5bc026c24c4d1b8662555132329548c80e982427
alphadan <info@alphabetsigns.com>
2022-11-17 11:29:57 -0500

    remove public API, then add locally

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAn6cm8cXGWS4z_VwkVLFKinAN5_oBFshU",
-  authDomain: "fir-asapi.firebaseapp.com",
-  databaseURL: "https://fir-asapi.firebaseio.com",
-  projectId: "fir-asapi",
-  storageBucket: "fir-asapi.appspot.com",
-  messagingSenderId: "114853796331",
-  appId: "1:114853796331:web:47a73bac0c42c606e1a1ae",
-};
-
-export const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
